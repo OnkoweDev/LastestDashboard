995e9ba3f512f266f6e6c1b299cfda5ce6adf924
William <anazawilliam1@gmail.com>
2023-04-08 04:25:45 +0100

    loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,20 +1,21 @@
-import React from 'react'
-import { Circles } from  'react-loader-spinner'
+import React from "react";
+import { Circles } from "react-loader-spinner";
 
 const Loader = () => {
   return (
+    // center align the loader or better still create a custom loader
     <div>
-        <Circles
-            height="80"
-            width="80"
-            color="#559fff"
-            ariaLabel="circles-loading"
-            wrapperStyle={{}}
-            wrapperClass=""
-            visible={true}            
-        />
+      <Circles
+        height="80"
+        width="80"
+        color="#559fff"
+        ariaLabel="circles-loading"
+        wrapperStyle={{}}
+        wrapperClass=""
+        visible={true}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default Loader
+export default Loader;
