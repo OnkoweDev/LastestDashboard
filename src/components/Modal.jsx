1614edb561dc47d257d4763ac550a96e03a46f20
William <anazawilliam1@gmail.com>
2023-04-20 20:54:11 +0100

    modal continue button done

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,9 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles/modal.css";
 
 import { IoClose } from "react-icons/io5";
 
 const Modal = ({ closeModal }) => {
+  const [count, setCount] = useState(0);
+
+  // modal contents
+  const modalContent = [
+    {
+      title: "Heading 1",
+      first:
+        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis, harum impedit similique, libero ex blanditiis explicabo expedita magni earum qui eum voluptas voluptatum, aut ipsum? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis, harum impedit similique, libero ex blanditiis explicabo expedita magni earum qui eum voluptas voluptatum, aut ipsum?",
+      second:
+        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis, harum impedit similique, libero ex blanditiis explicabo expedita magni earum qui eum voluptas voluptatum, aut ipsum? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis, harum impedit similique, libero ex blanditiis explicabo expedita magni earum qui eum voluptas voluptatum, aut ipsum?",
+    },
+    {
+      title: "Heading 2",
+      first:
+        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis, harum impedit similique, libero ex blanditiis explicabo expedita magni earum qui eum voluptas voluptatum, aut ipsum? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis, harum impedit similique, libero ex blanditiis explicabo expedita magni earum qui eum voluptas voluptatum, aut ipsum?",
+      second:
+        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis, harum impedit similique, libero ex blanditiis explicabo expedita magni earum qui eum voluptas voluptatum, aut ipsum? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis, harum impedit similique, libero ex blanditiis explicabo expedita magni earum qui eum voluptas voluptatum, aut ipsum?",
+    },
+    {
+      title: "Heading 3",
+      first:
+        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis, harum impedit similique, libero ex blanditiis explicabo expedita magni earum qui eum voluptas voluptatum, aut ipsum? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis, harum impedit similique, libero ex blanditiis explicabo expedita magni earum qui eum voluptas voluptatum, aut ipsum?",
+      second:
+        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis, harum impedit similique, libero ex blanditiis explicabo expedita magni earum qui eum voluptas voluptatum, aut ipsum? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis, harum impedit similique, libero ex blanditiis explicabo expedita magni earum qui eum voluptas voluptatum, aut ipsum?",
+    },
+  ];
+
+  const handleNext = (e) => {
+    e.preventDefault();
+    if (count < modalContent.length - 1) {
+      setCount((count) => (count += 1));
+      console.log(count);
+    } else {
+      setCount(modalContent.length - 1);
+    }
+  };
+
+  console.log(modalContent.length);
+
   return (
     <article className="modal__background" onClick={closeModal}>
       <div
@@ -14,28 +53,30 @@ const Modal = ({ closeModal }) => {
       >
         <IoClose className="close__modal__btn" onClick={closeModal} />
         <div className="modal__title">
-          <h2>How to guide on how to use the Olukowe App</h2>
+          <h2>{modalContent[count].title}</h2>
         </div>
         <div className="modal__body">
-          <p>
-            Lorem ipsum dolor sit, ok amet consectetur adipisicing elit.
-            Reiciendis, harum impedit similique, libero ex blanditiis explicabo
-            expedita magni earum qui eum voluptas voluptatum, aut ipsum?
-          </p>
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-            Reiciendis, harum impedit similique, libero ex blanditiis explicabo
-            expedita magni earum qui eum voluptas voluptatum, aut ipsum? Lorem
-            ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis,
-            harum impedit similique, libero ex blanditiis explicabo expedita
-            magni earum qui eum voluptas voluptatum, aut ipsum?
-          </p>
+          <p>{modalContent[count].first}</p>
+          <p>{modalContent[count].second}</p>
         </div>
+        {/* {modalContent.map((item) => {
+          return (
+            <div className="wrapper">
+              <div className="modal__title">
+                <h2>{item[count]?.title}</h2>
+              </div>
+              <div className="modal__body">
+                <p>{item[count].first}</p>
+                <p>{item[count].second}</p>
+              </div>
+            </div>
+          );
+        })} */}
         <div className="modal__footer">
           <button onClick={closeModal} id="cancle__btn">
             Skip
           </button>
-          <button>Continue</button>
+          <button onClick={handleNext}>Continue</button>
         </div>
       </div>
     </article>
