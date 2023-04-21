1a9e9c1412aab8ee491be59aeee22f18d8151e28
babawale123 <waleramson124@gmail.com>
2023-04-21 12:21:14 +0100

    dashboard

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,9 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles/modal.css";
 
 import { IoClose } from "react-icons/io5";
 
 const Modal = ({ closeModal }) => {
+  const [count, setCount] = useState(0);
+
+  // modal contents
+  const modalContent = [
+    {
+      title: "How it works",
+      first:
+        "1. Hey everyone! Are you ready to learn about the fascinating and mysterious world of SS? In this video, I'll show you how to use SS with the target keywords to get the most out of it. So let's get started and dive right in! 2. Hi there! Are you ready to explore the incredible world of SS? In this video, I'll show you how to use SS with the target keywords to take your experience to the next level. So let's get going and have some fun! 3. Hi everyone! Are you ready to unlock the power of SS? In this video, I'll show you how to use SS with the target keywords and get the most out of it. So let's get started and find out how SS can benefit you!",
+      second:
+        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis, harum impedit similique, libero ex blanditiis explicabo expedita magni earum qui eum voluptas voluptatum, aut ipsum? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis, harum impedit similique, libero ex blanditiis explicabo expedita magni earum qui eum voluptas voluptatum, aut ipsum?",
+    },
+    {
+      title: "Heading 2",
+      first:
+        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis, harum impedit similique, libero ex blanditiis explicabo expedita magni earum qui eum voluptas voluptatum, aut ipsum? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis, harum impedit similique, libero ex blanditiis explicabo expedita magni earum qui eum voluptas voluptatum, aut ipsum?",
+      second:
+        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis, harum impedit similique, libero ex blanditiis explicabo expedita magni earum qui eum voluptas voluptatum, aut ipsum? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis, harum impedit similique, libero ex blanditiis explicabo expedita magni earum qui eum voluptas voluptatum, aut ipsum?",
+    },
+    {
+      title: "Heading 3",
+      first:
+        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis, harum impedit similique, libero ex blanditiis explicabo expedita magni earum qui eum voluptas voluptatum, aut ipsum? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis, harum impedit similique, libero ex blanditiis explicabo expedita magni earum qui eum voluptas voluptatum, aut ipsum?",
+      second:
+        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis, harum impedit similique, libero ex blanditiis explicabo expedita magni earum qui eum voluptas voluptatum, aut ipsum? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis, harum impedit similique, libero ex blanditiis explicabo expedita magni earum qui eum voluptas voluptatum, aut ipsum?",
+    },
+  ];
+
+  const handleNext = (e) => {
+    e.preventDefault();
+    if (count < modalContent.length - 1) {
+      setCount((count) => (count += 1));
+      console.log(count);
+    } else {
+      setCount(modalContent.length - 1);
+    }
+  };
+
+  const handleBack = (e) => {
+    e.preventDefault();
+    if (count < modalContent.length + 1) {
+      setCount((count) => (count -= 1));
+      console.log(count);
+    } else {
+      setCount(modalContent.length + 1);
+    }
+  };
+
+  console.log(modalContent.length);
+
   return (
     <article className="modal__background" onClick={closeModal}>
       <div
@@ -14,28 +63,31 @@ const Modal = ({ closeModal }) => {
       >
         <IoClose className="close__modal__btn" onClick={closeModal} />
         <div className="modal__title">
-          <h2>How to guide on how to use the Olukowe App</h2>
+          <h2>{modalContent[count].title}</h2>
         </div>
         <div className="modal__body">
-          <p>
-            Lorem ipsum dolor sit, ok amet consectetur adipisicing elit.
-            Reiciendis, harum impedit similique, libero ex blanditiis explicabo
-            expedita magni earum qui eum voluptas voluptatum, aut ipsum?
-          </p>
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-            Reiciendis, harum impedit similique, libero ex blanditiis explicabo
-            expedita magni earum qui eum voluptas voluptatum, aut ipsum? Lorem
-            ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis,
-            harum impedit similique, libero ex blanditiis explicabo expedita
-            magni earum qui eum voluptas voluptatum, aut ipsum?
-          </p>
+          <p>{modalContent[count].first}</p>
+          <p>{modalContent[count].second}</p>
         </div>
+        {/* {modalContent.map((item) => {
+          return (
+            <div className="wrapper">
+              <div className="modal__title">
+                <h2>{item[count]?.title}</h2>
+              </div>
+              <div className="modal__body">
+                <p>{item[count].first}</p>
+                <p>{item[count].second}</p>
+              </div>
+            </div>
+          );
+        })} */}
         <div className="modal__footer">
+        <button onClick={count ? handleBack : closeModal}>back</button>
+          <button onClick={handleNext}>Next</button>
           <button onClick={closeModal} id="cancle__btn">
-            Skip
-          </button>
-          <button>Continue</button>
+          Skip
+        </button>
         </div>
       </div>
     </article>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,19 @@
 import React, { useState } from "react";
 import { SideNav, TopNav, Voice, HomepageData, Modal } from "../components";
 import "./styles/Home.css";
-// import youtube from "../assets/youtube.jpg";
-// import Facebook from "../assets/Facebook.jpg";
-// import blogIntro from "../assets/blogIntro.jpg";
-// import GmailLogo from "../assets/GmailLogo.png";
-// import blog from "../assets/blog.jpg";
-// import Rephraser from "../assets/Rephraser.jpg";
+
+import youtube from "../assets/icon/9.png";
+import facebook from "../assets/icon/facebook.png";
+import gmail from "../assets/icon/19.png";
+import blog from "../assets/icon/35.png";
+import linkedIn from "../assets/icon/7.png";
+import instagram from "../assets/icon/4.png";
+import twitter from "../assets/icon/2.png";
+import google from "../assets/icon/3.png";
+import feed from "../assets/icon/15.png";
+import audio from "../assets/icon/31.png";
+import image from "../assets/icon/36.png";
+
 import { Link } from "react-router-dom";
 
 import { FaFacebookF, FaYoutube, FaMailBulk, FaBloggerB } from "react-icons/fa";
@@ -54,129 +61,144 @@ const Home = () => {
           <SideNav />
           <div className="content">
             <div className="cards-container">
-            <div className="card">
-            <Link to="/all_youtube">
-              {/* <img src={youtube} /> */}
-              <FaYoutube className="icon" style={{ color: "red" }} />
-              <b>Youtube</b>
-            </Link>
-          </div>
-          <div className="card">
-            <Link to="/allfacebookads">
-              {/* <img src={Facebook} /> */}
-              <FaFacebookF className="icon" style={{ color: "#4267b2" }} />
-              <b>Facebook Ads</b>
-            </Link>
-          </div>
-          <div className="card">
-            <Link to="/email">
-              {/* <img src={GmailLogo} /> */}
-              <FaMailBulk className="icon" style={{ color: "#3357c0" }} />
-              <b>Email</b>
-            </Link>
-          </div>
+              <div className="card">
+                <Link to="/all_youtube">
+                  <img src={youtube} className="icon" />
+                  {/* <FaYoutube className="icon" style={{ color: "red" }} /> */}
+                  <b>Youtube</b>
+                </Link>
+              </div>
+              <div className="card">
+                <Link to="/allfacebookads">
+                  <img src={facebook} />
+                  {/* <FaFacebookF className="icon" style={{ color: "#4267b2" }} /> */}
+                  <b>Facebook Ads</b>
+                </Link>
+              </div>
+              <div className="card">
+                <Link to="/email">
+                  <img src={gmail} />
+                  {/* <FaMailBulk className="icon" style={{ color: "#3357c0" }} /> */}
+                  <b>Email</b>
+                </Link>
+              </div>
 
-          <div className="card">
-          <Link to="/allemailSubject">
-            {/* <img src={GmailLogo} /> */}
-            <FaMailBulk className="icon" style={{ color: "#3357c0" }} />
-            <b>Email Subject</b>
-          </Link>
-        </div>
-          
-          <div className="card">
-            <Link to="/all_intro">
-              {/* <img src={blogIntro} /> */}
-              <TfiWrite className="icon" style={{ color: "#002366" }} />
-              <b>Blog Intro</b>
-            </Link>
-          </div>
-          <div className="card">
-            <Link to="/allblogs">
-              {/* <img src={blog} /> */}
-              <FaBloggerB className="icon" style={{ color: "#fc4f08" }} />
-              <b>Blog Article</b>
-            </Link>
-          </div>
+              <div className="card">
+                <Link to="/allemailSubject">
+                  <img src={gmail} />
+                  {/* <FaMailBulk className="icon" style={{ color: "#3357c0" }} /> */}
+                  <b>Email Subject</b>
+                </Link>
+              </div>
 
-            <div className="card">
-              <Link to='/all_link_post'>
-              <BsLinkedin className="icon" style={{ color: "#4267b2" }} />
-              <b>Linkdlin Post</b>
-              </Link>
+              <div className="card">
+                <Link to="/all_intro">
+                  <img src={blog} />
+                  {/* <TfiWrite className="icon" style={{ color: "#002366" }} /> */}
+                  <b>Blog Intro</b>
+                </Link>
+              </div>
+              <div className="card">
+                <Link to="/allblogs">
+                  <img src={blog} />
+                  {/* <FaBloggerB className="icon" style={{ color: "#fc4f08" }} /> */}
+                  <b>Blog Article</b>
+                </Link>
+              </div>
+
+              <div className="card">
+                <Link to="/all_link_post">
+                  <img src={linkedIn} alt="" />
+                  {/* <BsLinkedin className="icon" style={{ color: "#4267b2" }} /> */}
+                  <b>Linkdlin Post</b>
+                </Link>
+              </div>
+
+              <div className="card">
+                <Link to="/all_paragraph">
+                  {/* <img src={instagram} alt="" /> */}
+                  <FaParagraph className="icon" style={{ color: "#4267b2" }} />
+                  <b>Paragraph</b>
+                </Link>
+              </div>
+
+              <div className="card">
+                <Link to="/all_ebook">
+                  <GiWhiteBook className="icon" style={{ color: "#4267b2" }} />
+                  <b>Ebook</b>
+                </Link>
+              </div>
+
+              <div className="card">
+                <Link to="/all_linkedin_ads">
+                  <img src={linkedIn} alt="" />
+                  {/* <AiOutlineLinkedin
+                    className="icon"
+                    style={{ color: "#4267b2" }}
+                  /> */}
+                  <b>Linkdlin Ads</b>
+                </Link>
+              </div>
+
+              <div className="card">
+                <Link to="/allinstagram">
+                  <img src={instagram} alt="" />
+                  {/* <FiInstagram className="icon" style={{ color: "#4267b2" }} /> */}
+                  <b>Instagram Post</b>
+                </Link>
+              </div>
+
+              <div className="card">
+                <Link to="/all_tweet">
+                  <img src={twitter} alt="" />
+                  {/* <BsTwitter className="icon" style={{ color: "#4267b2" }} /> */}
+                  <b>Twitter Post</b>
+                </Link>
+              </div>
+
+              <div className="card">
+                <Link to="/allgoogleads">
+                  <img src={google} alt="" />
+                  {/* <GrGoogle className="icon" style={{ color: "#4267b2" }} /> */}
+                  <b>Google Ads</b>
+                </Link>
+              </div>
+
+              <div className="card">
+                <Link to="/alltitle">
+                  <img src={google} alt="" />
+                  {/* <FaGooglePlus className="icon" style={{ color: "#4267b2" }} /> */}
+                  <b>Google Title</b>
+                </Link>
+              </div>
+
+              <div className="card">
+                <Link to="/blogsection">
+                  <img src={feed} alt="" />
+                  {/* <FaBlog className="icon" style={{ color: "#4267b2" }} /> */}
+                  <b>Blog Section</b>
+                </Link>
+              </div>
+
+              <div className="card">
+                <Link to="/all_image">
+                  <img src={image} alt="" />
+                  {/* <BsImages className="icon" style={{ color: "#4267b2" }} /> */}
+                  <b>Images</b>
+                </Link>
+              </div>
+
+              <div className="card">
+                <Link to="/all_audio">
+                  <img src={audio} alt="" />
+                  {/* <AiTwotoneAudio
+                    className="icon"
+                    style={{ color: "#4267b2" }}
+                  /> */}
+                  <b>Transcribed Audio</b>
+                </Link>
+              </div>
             </div>
-
-            <div className="card">
-            <Link to='/all_paragraph'>
-              <FaParagraph className="icon" style={{ color: "#4267b2" }} />
-              <b>Paragraph</b>
-            </Link>
-          </div>
-
-            <div className="card">
-            <Link to='/all_ebook'>
-            <GiWhiteBook className="icon" style={{ color: "#4267b2" }} />
-            <b>Ebook</b>
-            </Link>
-          </div>
-
-          <div className="card">
-          <Link to="/all_linkedin_ads">
-          <AiOutlineLinkedin className="icon" style={{ color: "#4267b2" }} />
-          <b>Linkdlin Ads</b>
-          </Link>
-        </div>
-
-        <div className="card">
-          <Link to="/allinstagram">
-          <FiInstagram className="icon" style={{ color: "#4267b2" }} />
-          <b>Instagram Post</b>
-          </Link>
-        </div>
-
-        <div className="card">
-        <Link to="/all_tweet">
-        <BsTwitter className="icon" style={{ color: "#4267b2" }} />
-        <b>Twitter Post</b>
-        </Link>
-      </div>
-
-        <div className="card">
-        <Link to="/allgoogleads">
-        <GrGoogle className="icon" style={{ color: "#4267b2" }} />
-        <b>Google Ads</b>
-        </Link>
-      </div>
-
-      <div className="card">
-      <Link to="/alltitle">
-      <FaGooglePlus className="icon" style={{ color: "#4267b2" }} />
-      <b>Google Title</b>
-      </Link>
-    </div>
-
-    <div className="card">
-    <Link to="/blogsection">
-    <FaBlog className="icon" style={{ color: "#4267b2" }} />
-    <b>Blog Section</b>
-    </Link>
-  </div>
-
-  <div className="card">
-  <Link to="/all_image">
-  <BsImages className="icon" style={{ color: "#4267b2" }} />
-  <b>Images</b>
-  </Link>
-</div>
-
-
-<div className="card">
-<Link to="/all_audio">
-<AiTwotoneAudio className="icon" style={{ color: "#4267b2" }} />
-<b>Transcribed Audio</b>
-</Link>
-</div>
-</div>
             {/* <Voice /> */}
           </div>
         </div>
