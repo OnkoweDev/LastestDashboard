730d1b1529ab1fa8691994e8b985724fb3a0e90e
William <anazawilliam1@gmail.com>
2023-04-29 17:13:50 +0100

    modal fixed

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles/modal.css";
 
 import { IoClose } from "react-icons/io5";
 
 const Modal = ({ closeModal }) => {
+  const [count, setCount] = useState(0);
+  const modalContent = [
+    { title: "title1", content1: "content1", content2: "content2" },
+    { title: "title2", content1: "content3", content2: "content4" },
+    { title: "title3", content1: "content5", content2: "content6" },
+  ];
+
+  const handleNext = () => {
+    if (count < modalContent.length - 1) {
+      setCount((count) => (count += 1));
+    }
+  };
+
+  const handlePrev = () => {
+    if (count > 0) {
+      setCount((count) => (count -= 1));
+    }
+  };
+
   return (
     <article className="modal__background" onClick={closeModal}>
       <div
@@ -13,29 +32,21 @@ const Modal = ({ closeModal }) => {
         }}
       >
         <IoClose className="close__modal__btn" onClick={closeModal} />
-        <div className="modal__title">
-          <h2>How to guide on how to use the Olukowe App</h2>
-        </div>
-        <div className="modal__body">
-          <p>
-            Lorem ipsum dolor sit, ok amet consectetur adipisicing elit.
-            Reiciendis, harum impedit similique, libero ex blanditiis explicabo
-            expedita magni earum qui eum voluptas voluptatum, aut ipsum?
-          </p>
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-            Reiciendis, harum impedit similique, libero ex blanditiis explicabo
-            expedita magni earum qui eum voluptas voluptatum, aut ipsum? Lorem
-            ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis,
-            harum impedit similique, libero ex blanditiis explicabo expedita
-            magni earum qui eum voluptas voluptatum, aut ipsum?
-          </p>
-        </div>
+        <section className="modal__wrapper">
+          <header className="modal__title">
+            <h2>{modalContent[count].title}</h2>
+          </header>
+          <article className="modal__body">
+            <p>{modalContent[count]?.content1}</p>
+            <p>{modalContent[count]?.content2}</p>
+          </article>
+        </section>
         <div className="modal__footer">
+          <button onClick={handlePrev}>Prev</button>
           <button onClick={closeModal} id="cancle__btn">
             Skip
           </button>
-          <button>Continue</button>
+          <button onClick={handleNext}>Next</button>
         </div>
       </div>
     </article>
