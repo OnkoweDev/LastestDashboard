313d978f9995e5e59f4d6367682a18dc535cc36f
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-12-28 07:09:42 +0100

    Feat: Revamped login UI

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,134 +1,86 @@
-import React, { useEffect, useState } from "react";
-import "./styles/SignInUp.css";
-
-import { AiOutlineEyeInvisible, AiOutlineEye } from "react-icons/ai";
+import React, { useState } from "react";
+import Button from "../components/Button";
+import { Link } from "react-router-dom";
 import loginImage from "../assets/signInImage.png";
-import logo from "./../assets/logo.png";
-
-import { Link, useNavigate } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
-import { login } from "../actions/userAction";
 import { FcGoogle } from "react-icons/fc";
 import { BsFacebook } from "react-icons/bs";
 import { FaTwitter } from "react-icons/fa";
-import Button from "../components/Button";
 
 const SignIn = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [message, setMessage] = useState("");
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-  const userLogin = useSelector((state) => state.userLogin);
-  const { loading, userInfo, error } = userLogin;
-
-  const handleLogin = (e) => {
-    e.preventDefault();
-    dispatch(login(email, password));
-  };
-
-  useEffect(() => {
-    try {
-      if (userInfo) {
-        navigate("/dashboard");
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  }, [userInfo]);
-
-  const [showPassowrd, setShowPassword] = useState(false);
-  const showPassword = () => {
-    setShowPassword(!showPassowrd);
-  };
   return (
-    <div className="sign__container bg-[#FCFCFC] ">
-      <img src={logo} alt="Logo" className="logo" />
-      <section className="form__section">
-        <div className="form__section__wrapper">
-          <h5 className="font-bold">Welcome Back!</h5>
-          <br />
-          <small>Ready to gather the thought once again?</small>
+    <section className="grid md:grid-cols-2  w-screen h-screen">
+      <section className="form-section w-full h-full flex items-center justify-center flex-col">
+        <section className="flex items-center justify-center flex-col gap-y-2">
+          <h1 className="text-[#559fff] font-bold text-4xl text-center capitalize">
+            welcome back!
+          </h1>
+          <p className="">Ready to gather the thought once again?</p>
+        </section>
 
-          {error && <div className=" bar error">{error}</div>}
-          <form onSubmit={handleLogin}>
-            <label htmlFor="email" className="py-2">
-              Email Address
+        <br />
+        <form className="lg:w-9/12 p-5">
+          <section className="my-3">
+            <label htmlFor="email" className="capitalize my-2">
+              email address
             </label>
             <input
-              required
-              placeholder="email"
-              type="email"
-              name="email"
               className="p-4 w-full border-[1px] border-black rounded-[8px] focus:border-transparent focus:outline-none focus:ring-2 focus:ring-blue-300"
-              onChange={(e) => setEmail(e.target.value)}
-              value={email}
+              placeholder="Enter your email address"
+              name="email"
             />
+          </section>
 
-            <label htmlFor="Password" className="py-2">
-              Password
+          <section className="my-3">
+            <label htmlFor="password" className="capitalize my-2">
+              password
             </label>
-            <div className="input__container">
-              <article className="input__wrapper">
-                <input
-                  required
-                  type={showPassowrd ? "text" : "password"}
-                  className="p-4 w-full border-[1px] border-black rounded-[8px] focus:border-transparent focus:outline-none focus:ring-2 focus:ring-blue-300 flex items-center"
-                  placeholder="password"
-                  onChange={(e) => setPassword(e.target.value)}
-                  value={password}
-                />
-                {showPassowrd ? (
-                  <AiOutlineEyeInvisible
-                    className="icon w-5 h-5"
-                    onClick={showPassword}
-                  />
-                ) : (
-                  <AiOutlineEye
-                    className="icon w-5 h-5"
-                    onClick={showPassword}
-                  />
-                )}
-              </article>
-            </div>
-            {/* btn */}
-            <Button type="submit" className="my-3 mb-5">
-              {" "}
-              {loading ? "Authenticating...." : "Login"}
-            </Button>
-          </form>
-          {/* other sign up options */}
-          <p className="text-center my-3">Or Log in with </p>
+            <input
+              className="p-4 w-full border-[1px] border-black rounded-[8px] focus:border-transparent focus:outline-none focus:ring-2 focus:ring-blue-300"
+              placeholder="Enter your password"
+              name="password"
+            />
+          </section>
 
-          <div className="sign__up__options">
-            <button style={{ color: "red", size: "50px" }}>
-              {" "}
-              <FcGoogle style={{ width: "30px", height: "30px" }} />
-            </button>
-            <button style={{ color: "#4267B2" }}>
-              <BsFacebook style={{ width: "30px", height: "30px" }} />
-            </button>
-            <button style={{ color: "#1DA1F2" }}>
-              <FaTwitter style={{ width: "30px", height: "30px" }} />
-            </button>
-          </div>
+          <br />
 
-          <p className="text-center m-5">
-            Don’t have an account?{" "}
-            <Link to="/sign-up">
-              <span className="text-blue-100">Sign Up</span>
-            </Link>
-            <br />
-            <Link to="/forgetpassword">
-              <span className="text-blue-100">Forgot password</span>
-            </Link>
-          </p>
-        </div>
+          <section>
+            <Button>Login</Button>
+          </section>
+
+          <br />
+
+          <section className="flex w-full items-center justify-center flex-col p-2">
+            <p className="text-center capitalize">or login with</p>
+
+            <section className="border-[1px] border-black w-96 rounded-lg flex items-center justify-around p-2 my-4">
+              <FcGoogle className="w-8 h-8 cursor-pointer" />
+              <BsFacebook className="w-8 h-8 text-[#4267B2] cursor-pointer" />
+              <FaTwitter className="w-8 h-8 text-[#1DA1F2] cursor-pointer" />
+            </section>
+          </section>
+
+          <section>
+            <p className="text-center m-5">
+              Don’t have an account?{" "}
+              <Link to="/sign-up">
+                <span className="text-blue-100">Sign Up</span>
+              </Link>
+              <br />
+              <Link to="/forgetpassword">
+                <span className="text-blue-100">Forgot password</span>
+              </Link>
+            </p>
+          </section>
+        </form>
       </section>
-      <section className="md:block hidden">
-        <img src={loginImage} alt="" />
+      <section className="image-section w-full md:flex hidden justify-end overflow-hidden">
+        <img
+          src={loginImage}
+          alt="login image"
+          className="object-contain h-full"
+        />
       </section>
-    </div>
+    </section>
   );
 };
 
